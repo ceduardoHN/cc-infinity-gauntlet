eebf2c9fce9bb90875acfd57b60b2e9adfed4314
Kenneth Toxcon <kennethtoxro@gmail.com>
2023-02-21 18:45:57 -0600

    feat: adding api config

---
+++ community-tesseract/src/index.js
@@ -0,0 +1,16 @@
+import cors from "cors";
+import express from "express";
+import { initDB } from "./db/index.js";
+
+const api = express();
+
+const apiPort = process.env["APP_ENV"] || 3000;
+
+api.use(cors());
+api.use(express.json());
+api.use(express.urlencoded({ extended: false }));
+
+api.listen(apiPort, () => {
+  console.log(`API RUNNNIG ON PORT ${apiPort}`);
+  initDB().then(() => console.log("DB INITIALIZED :)"));
+});
